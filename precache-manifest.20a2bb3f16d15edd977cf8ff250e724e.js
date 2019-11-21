1b853d2f7bca0ac49e16d76fa288cd3501f00901
CiroLee <lizegang@sdbattery.com>
2019-11-21 15:47:30 +0800

    Updates

---
+++ precache-manifest.20a2bb3f16d15edd977cf8ff250e724e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28827755242e5684250b649439f521d",
+    "url": "/CiroLee/myblogs/index.html"
+  },
+  {
+    "revision": "a5fa9562e68e4f281e7d",
+    "url": "/CiroLee/myblogs/static/css/main.f360e64d.chunk.css"
+  },
+  {
+    "revision": "bdf652cd6efa2cf6fe1e",
+    "url": "/CiroLee/myblogs/static/js/2.5817040d.chunk.js"
+  },
+  {
+    "revision": "a5fa9562e68e4f281e7d",
+    "url": "/CiroLee/myblogs/static/js/main.1da79d57.chunk.js"
+  },
+  {
+    "revision": "27d03958d010aa74b468",
+    "url": "/CiroLee/myblogs/static/js/runtime-main.9cff253b.js"
+  }
+]);
